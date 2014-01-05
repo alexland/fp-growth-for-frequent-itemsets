84f37b71f06da29fb97d62238853d912283623ec
doug y'barbo <doug.ybarbo@gmail.com>
2014-01-05 11:21:26 -0800

    my template config file for pytest

---
+++ conftest.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+import pytest
+
+
+def test_compute(param1): 
+	assert param1 < 4
+	
+	
+#---------- content of conftest.py ----------#
+
+def pytest_addoption(parser): 
+	parser.addoption("--stringinput", action="append", default=[], help="list of str inputs for test fns")
+	
+	
+def pytest_generate_tests(metafunc):
+	if 'stringinput' in metafunc.fixturenames:
+		metafunc.paramaterize("stringinput", metafunc.config.option.stringinput)
+		
