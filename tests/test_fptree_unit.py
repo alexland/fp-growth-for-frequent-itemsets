a791a2f5481ceb055137f973e1357753f41dd5ba
doug y'barbo <doug.ybarbo@gmail.com>
2014-01-08 17:37:30 -0800

    synced paramater lists caused by changes in main module

--- tests/test_fptree_unit.py
+++ tests/test_fptree_unit.py
@@ -7,6 +7,7 @@ import itertools as IT
 
 import pytest
 import fptree as FPT
+import fptree_query as FPQ
 
 #-------------------------- test set-up & tear-down -----------------#
 
@@ -47,22 +48,56 @@ data2 = [
 
 #------------------------- fixtures --------------------------------#
 
+@pytest.fixture(scope="module")
+def myFixture1():
+	return data1
+
+
+@pytest.fixture(scope="module")
+def myFixture2():
+	return data2
+
+@pytest.fixture(scope="module", params=["param1", "param2"])
+def myFixture3(request):
+	if request.param == "param1":
+		return request.getfuncargvalue("myFixture1")
+	elif request.param == "param2":
+		return request.getfuncargvalue("myFixture2")
+
+
 def item_counter(dataset):
 	dataset = [set(trans) for trans in dataset]
 	# flatten the data (from list of sets to list of items)
-	trans_flat = [itm for trans in dataset for itm in trans]
+	trans_flat = [item for trans in dataset for itm in trans]
 	# get frequency of each item
 	ic = CL.defaultdict(int)
-	for itm in trans_flat:
-		ic[itm] += 1
+	for item in trans_flat:
+		ic[item] += 1
 	return ic
 
+
+def build_fptree(data):
+	fptree = FPT.TreeNode('root', None)
+	root = fptree
+	htab, transactions = FPT.config_fptree_builder(data, len(data))
+	for trans in transactions:
+		FPT.add_nodes(trans, htab, root)
+	htab = {k:v[:2] for k, v in htab.items()}
+	return fptree, htab
 
 #------------------------- test fns --------------------------------#
 
 
 
 #------------------------- assertions --------------------------------#
+
+def test_1(myFixture1):
+	assert len(myFixture1) == 8
+
+
+def test_3(myFixture3):
+	assert myFixture3 in (data1, data2)
+
 
 def test_item_counter():
 	ic = FPT.item_counter(data1)
@@ -96,5 +131,35 @@ def test3_build_min_spt_filter_str():
 	assert FPT.build_min_spt_filter_str(excl_items) == '(q=="A") | (q=="B") | (q=="C") | (q=="D") | (q=="E") | (q=="F")'
 
 
+# unit tests for fptree_query:
+
+def test2_f_list():
+	trans_count = len(data1)
+	min_spt = 0.3
+	item = "C"
+	_, htab = build_fptree(data1)
+	_, flist = FPQ.f_list(item, htab, min_spt, trans_count)
+	assert len(flist.items()) == 1
+
+
+def test2_f_list():
+	trans_count = len(data1)
+	min_spt = 0.3
+	item = "C"
+	_, htab = build_fptree(data1)
+	_, flist= FPQ.f_list(item, htab, min_spt, trans_count)
+	assert list(flist.values())[0] == 3
+
+
+def test3_f_list():
+	trans_count = len(data1)
+	min_spt = 0.3
+	item = "C"
+	_, htab = build_fptree(data1)
+	_, flist = FPQ.f_list(item, htab, min_spt, trans_count)
+	assert list(flist.keys())[0] == 'B'
+
+
+ 
 
 
