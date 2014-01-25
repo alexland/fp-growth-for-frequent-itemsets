eb2bb6b79a65c22adf5de6efd9083827cc145143
doug y'barbo <doug.ybarbo@gmail.com>
2014-01-25 14:49:37 -0800

    added the fns used to compose main recursive fn [ci skip]

--- fptree_query.py
+++ fptree_query.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+from copy import deepcopy
+import collections as CL
+from functools import partial
 import fptree as FPT
 import fptree_query_utils as FQU
 
 
-# temporary container for frequent itemsets
 frequent_itemsets = []
 
 
-def count_frequent_itemsets(freq_itemsets):
+def count_frequent_itemsets(frequent_itemsets):
 	"""
 	returns:
 	pass in:
 	"""
-	fnx = lambda g: ''.join(g)
-	tx = map(fnx, freq_itemsets)
 	cx = CL.defaultdict(int)
-	for s in tx:
+	for s in frequent_itemsets:
 		cx[s] += 1
 	return cx
-
-
 
 #------------------ recursively find frequent itemsets  --------------------#
 
@@ -29,56 +27,53 @@ def count_frequent_itemsets(freq_itemsets):
 # then if cfptree has more than one node in it:
 # repeat the mining loop; ELSE: exit
 
-def find_frequent_itemsets(item, fptree, dataset, min_spt, trans_count,
-	header_table=FPT.htab, frequent_itemsets=[]):
+def persist_frequent_itemsets(item, cpb_all, frequent_itemsets):
 	"""
-	returns: None, recursively builds conditional fptrees, mine
-		them for frequent item sets then adds them to frequent_itemsets
+	returns: None, populates frequent itemset container with
+		filtered conditional pattern bases, to which the
+		original transaction element (route start) is first appended,
+		then pattern base is converted from list to string
 	pass in:
-		(i) empty container (frequent_itemsets)
+		(i) unique transaction item
+		(ii) filtered conditional pattern bases (returned from
+			filter_cpbs_by_flist)
+		(ii) container storing all frequent itemsets
 	"""
-	# mine tree for frequent itemsets
-	cpb_all = FQU.get_conditional_pattern_bases(item, header_table)
-	f_list = FQU.create_flist(dataset, cpb_all, min_spt)
-	cpb_all = FQU.filter_cpbs_by_flist(cpb_all, f_list, min_spt, trans_count,
-		header_table)
-	# add frequent pattern bases to container
-		# after the cpbs are filtered but *before* they are re-ordered:
-	for fi in cpb_all:
-		fi.append(item)
-		frequent_itemsets.append(fi)
-
-	cpb_all = FQU.sort_cpbs_by_freq(cpb_all, dataset)
-	return frequent_itemsets
+	fnx = lambda g: ''.join(g)
+	fis = deepcopy(cpb_all)
+	for item_set in fis:
+		item_set.append(item)
+		item_set = fnx(item_set)
+		frequent_itemsets.append(item_set)
+	return None
 
 
+def find_frequent_itemsets(item, fptree, dataset, min_spt, trans_count, header_table=FPT.htab):
+    cpb_all = FQU.get_conditional_pattern_bases(item, header_table)
+    f_list = FQU.create_flist(dataset, cpb_all, min_spt)
+    cpb_all = FQU.filter_cpbs_by_flist(cpb_all, f_list)
+    persist_frequent_itemsets(item, cpb_all, frequent_itemsets)
+    cpb_all = FQU.sort_cpbs_by_freq(cpb_all, dataset)
+    return frequent_itemsets
 
-# examine cfptree to determine which case below applies
 
-# base case:
-# return
+item = 'C'
+fptree = FPT.fptree
+dataset = FPT.data0
+min_spt = 0.3
+trans_count = len(dataset)
 
-# recursive case:
-# return find_frequent_itemsets(cfptree, frequent_itemsets=frequent_itemsets)
+p_find_frequent_itemsets = partial(find_frequent_itemsets,
+	min_spt=min_spt, trans_count=len(dataset))
+
+
 
 
 unique_transaction_items = list(FPT.htab.keys())
 
-item = 'E'
-min_spt = 0.3
-header_table = FPT.htab
-dataset = FPT.data0
-trans_count = len(dataset)
+for item in unique_transaction_items:
+	p_find_frequent_itemsets(item, fptree, dataset)
 
-cpb_all = FQU.cpb_all
-f_list = FQU.f_list
-cpb_all_filtered = FQU.cpb_all_filtered
-cpb_all_filtered_sorted = FQU.cpb_all_filtered_sorted
 
-# find_frequent_itemsets('E', FPT.fptree, dataset, min_spt, trans_count, FPT.htab)
-
-# for t in unique_transaction_items:
-# 	find_frequent_itemsets()
-
-# add filtered conditional pattern bases to a temp container
-	# count_frequent_itemsets(cpb_all_filtered)
+FIS = frequent_itemsets
+header_table FPT.htab
