8cd736eff735ecbff9b5524e3552a8e57e69cca3
doug y'barbo <doug.ybarbo@gmail.com>
2014-02-24 10:56:12 -0800

    various minor revisions [ci skip]

--- fptree_query.py
+++ fptree_query.py
@@ -7,72 +7,57 @@ from functools import partial
 
 import fptree as FPT
 import fptree_query_utils as FQU
+import fptree_query as FPQ
 
 
-#------------------ recursively find frequent itemsets  --------------------#
-
-# call build_conditional_fptree, once for each unique item in htab.keys(),
-# then if cfptree has more than one node in it:
-# repeat the mining loop; else: exit
-
-FP = []
-
-
-def gather_nodes(node, N=[]):
-    nx = node.children
-    if len(nx) == 0:
-        return N
-    else:
-        for n in nx.keys():
-            N.append(n)
-            gather_nodes(node.children[n], N)
-    return N
-
-
-def persist_freq_patterns(path, f_list):
-    k = ''.join(path)
-    q = path[-1]
-    v = f_list[q]
-    return {k:v}
-
-
-def cpbs(k, header_table, MIN_SPT, trans_count):
-	cpb_all = get_conditional_pattern_bases(k, header_table=header_table)
-	if len(cpb_all) == 0:
-		return None
-	else:
-		f_list = create_flist(cpb_all=cpb_all, min_spt=MIN_SPT,trans_count=trans_count)
-		cpb_all = filter_cpb_by_flist(cpb_all, f_list)
-		cpb_all = sort_cpb_by_freq(cpb_all)
-		cpb_all = deepcopy(list(cpb_all))
-	return cpb_all, f_list
-
-FIS = []
+FIS = FPQ.FIS
 
 
 
-def mine_tree(p=CL.deque([]), header_table=FPT.htab, min_spt=FPT.MIN_SPT, trans_count=len(FPT.dataset),
-              c=0, f_list=FPT.htab):
-    for k in f_list.keys():
-        p1 = deepcopy(p)
-        p1.appendleft(k)
-        q = ''.join(p1)
-        x = cpbs(k, header_table, MIN_SPT, trans_count)
-        if x:
-            cpb_all_, f_list_ = x
-            q = ''.join(p1)
-            print('path: {}'.format(q))
-            if len(q) > 1:
-                fis = q + ':' + str(header_table[k][0])
-                print('fis: {}: {}'.format(q, f_list[k]))
-                FIS.append(fis)
-            cfptree, chtab = build_fptree(cpb_all_, len(cpb_all_), MIN_SPT, k)
-            mine_tree(p=p1, header_table=chtab, min_spt=MIN_SPT, trans_count=trans_count,
-                      c=c+5, f_list=f_list_)
-        elif not x:
-            q = ''.join(p1)
-            if len(q) > 1:
-                fis = q + ':' + str(header_table[k][0])
-                print('fis: {}: {}'.format(q, header_table[k][0]))
-                FIS.append(fis)
-            continue
+
+def create_fis_containers(FIS):
+	""""
+	returns: two containers that store the same data, all freq
+		itemsets for a given dataset
+	pass in: sequence of all frequent itemsets, each fis is
+		a string of the form fis:count, eg, 'xyz:99'
+	"""
+	fis_all = [itm.strip().split(':') for itm in FIS]
+	fis =  [list(t[0]) for t in fis_all]
+	fis_cnt = {k:v for k, v in fis_all}
+
+
+
+
+def itemset_begins_with(probe, fis = FIS):
+    p = r" " + re.escape(probe) + r""
+    p = r"{} {} {} ".format(a, b, c)
+    po = re.compile(p)
+    po.findall
+
+
+def itemsets_begin_with():
+    p = r'^(A.*?)$'
+    po = re.compile(p)
+    for line in FIS:
+        m = po.search(line)
+        if m:
+            print(m.group(1))
+
+itemsets_begin_with()
+
+def itemsets_begin_with(probe):
+    return [ line for line in FIS if line.startswith(probe) ]
+
+
+
+p = r'^([A-Z][^A-Z])$'
+p = r'^([A-Z]?)(?![A-Z])'
+po = re.compile(p)
+
+fis = [ itm for itm in FIS if not po.search(itm) ]
+
+p1 = r'\d'
+po1 = re.compile(p1)
+
+fis = [ po1.sub('', itm) for itm in fis ]
