cf2d80a5d92416a8057edad99120c185be173ae3
doug y'barbo <doug.ybarbo@gmail.com>
2014-03-06 14:55:19 -0800

    fixed code causing bad file paths

--- fptree_build.py
+++ fptree_build.py
@@ -20,38 +20,12 @@ from functools import wraps
 
 import fptree_config as FPC
 
-def load_data(dfile=None, max_transactions=250):
-	import random as RND
-	if dfile and dfile.endswith('.csv'):
-		with open(dfile, 'r', encoding='utf-8') as fh:
- 			reader = CSV.reader(fh)
- 			return [line for line in reader]
-
-	elif dfile and not dfile.endswith('.csv'):
-		with open(dfile, "r", encoding="utf-8") as fh:
-			data = [ line.strip().split(' ') for line in fh.readlines()
-				if not line.startswith('#') ]
-			RND.shuffle(data)
-			data = data[:max_transactions]
-	else:
-		import string
-		import random as RND
-		p = list(string.ascii_uppercase[:20])
-		fnx = lambda: RND.randint(2, 10)
-		data = [ RND.sample(p, fnx()) for c in range(100000) ]
-		RND.shuffle(data)
-		if max_transactions & max_transactions < len(data):
-			return data[:max_transactions]
-		else:
-			return data
-
-
 configs_filename = 'config-t6.json'
 configs = FPC.get_configs(configs_filename)
 MIN_SPT = configs['min_support']
 MIN_FREQ_ITEMSET_LENGTH = configs['min_freq_itemset_length']
 # dataset = load_data()
-dataset = load_data(configs['data_file'])
+dataset = FPC.load_data(configs['data_file'])
 TRANS_COUNT = len(dataset)
 
 
